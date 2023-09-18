538741b000b7095e1ac8427d5565dc37421951b3
MichaelMoranis <michaelmoranesul@gmail.com>
2023-09-18 18:18:13 -0300

    excluindo arquivos index.css e app.css

--- portifolio-vite/src/main.jsx
+++ portifolio-vite/src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './routes'
 import theme from './components/theme'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
