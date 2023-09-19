c24dfa0568a2da9233d1d895cb870766caba7934
MichaelMoranis <michaelmoranesul@gmail.com>
2023-09-19 17:20:14 -0300

    configuracao das rotas absolut imports

--- portifolio-vite/src/main.jsx
+++ portifolio-vite/src/main.jsx
@@ -2,7 +2,7 @@ import {  ColorModeScript } from '@chakra-ui/react';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from './routes'
-import theme from './components/theme'
+import theme from 'components/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
